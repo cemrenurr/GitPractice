abe4c37d31a1bd10553d5dfb5ccc90e28643b611
Cemrenur Topuz <cemre.topuz@gmail.com>
2019-11-22 14:49:10 -0500

    first commit

---
+++ src/test/java/gitpractice.java
@@ -0,0 +1,18 @@
+public class gitpractice {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    /*
+    Git init-> create repository we use only once.
+    Git status->
+    Git ignore-> tells git which files should be ignored by git. reports, target folder, some output folders should be ignored.
+    target-> created automatically once we run the program. They should not be tracked by git.
+             do not push target folder ever!
+
+    Create git ignore file:
+    1-right click on the project
+    2-
+
+
+     */
+}
